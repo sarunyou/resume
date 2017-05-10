e24d712dacf29f4a780a2efde8530395a3ea6fd2
Sarunyou W <sarunyouwhang11@gmail.com>
2017-05-10 14:32:51 +0700

    created parallex first time

--- src/Header.js
+++ src/Header.js
@@ -4,16 +4,35 @@ import './Header.css';
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.scrolled = 0;
+    this.state = {
+      scrollTop : 0
+    };
+    this.handleScroll = this.handleScroll.bind(this);
   }
+
+  componentDidMount() {
+    setInterval(() => {
+      window.addEventListener('scroll', this.handleScroll);
+    }, 100);
+  }
+
+  handleScroll(e) {
+    console.log('scrolling');
+    const scrollTop = document.body.scrollTop
+    this.setState({
+      scrollTop
+    });
+  }
+  
   render() {
+    const transltePanda = {transform: "translateY(-" + this.state.scrollTop / 10 + "px)"};
+    const transltePortfoliologo = {transform: "translateY(" + this.state.scrollTop / 2 + "px)"};
     return (
       <header>
         <div className="bg-blur">
-          <div className="portfolio-logo"></div>
-          <div className="panda-img"></div>
+          <div className="portfolio-logo" style={transltePortfoliologo}></div>
+          <div className="panda-img" style={transltePanda}></div>
           <div className="rock-img"></div>
-          {/*<div className="profile-img"></div>*/}
         </div>
       </header>
     );
